f491ef40c4d8207aea0fcb30f93c1989612b2216
XicoAntonio <fm.antonio@campus.fct.unl.pt>
2018-09-27 00:01:41 +0100

    funciona sem threads, problema com retorno de threads

--- aula2.c
+++ aula2.c
@@ -13,32 +13,35 @@ void *f(void * arg){
 	double a = 0;
 	double b = 0;
 	struct drand48_data buffer;
-	
+	printf("y: %i\n", y);
 	for(int i = 0; i < y; i++){
+		
 		drand48_r(&buffer, &a);
 		drand48_r(&buffer, &b);
-		
-		if( (a*a + b*b) < 1){
+		if( (a*a + b*b) <= 1){
 			(*count)++;
 		}	
 	}
-
+	printf("hits: %i\n", *count);
 	return (void*)count;
 }
 
 int main (int argc, char *argv[]) 
 {
 
-	int shots = *(int*) argv[1];
-	double res;
+	int shots = atoi(argv[1]);	
+	int hits;
 	if(argc > 2){
 		int threads = *(int*) argv[2];
 		pthread_t th2;
 		pthread_create(&th2, NULL, f, (void *) &shots);
-	}else{
-		res = *(double*)f((void*)&shots);
-	}
 
-	printf("%f\n", res*4);
+		pthread_join(th2, (void*)&hits);
+	}else{
+		hits = *(int*)f((void*)&shots);
+	}
+	
+	printf("hits: %i\n", hits);
+	printf("%f\n", (double)(hits*4)/shots);
 	return 0;
 }
