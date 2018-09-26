5864ba35bb7fdbbc445689768599423498a58caf
XicoAntonio <fm.antonio@campus.fct.unl.pt>
2018-09-26 18:47:24 +0100

    sem erros

--- aula2.c
+++ aula2.c
@@ -1,21 +1,8 @@
 #include <stdlib.h>  
-#include <drand48_r_libgw32c.h>
+#include <stdio.h> 
+#define _GNU_SOURCE
+#include <pthread.h>
 
-int main (int argc, char *argv[]) 
-{
-
-	int shots = (int) argv[1];
-	if(argc > 2){
-		int threads = (int) argv[2];
-		pthread_t th2;
-		pthread_create(&th2, NULL, f, (void *) &shots)
-	}else{
-		double res = f(&shots)
-	}
-
-	printf("%f\n", res*4);
-	return 0;
-}
 
 void *f(void * arg){
 
@@ -23,12 +10,13 @@ void *f(void * arg){
 	
 	int y = *(int*)arg;
 
-	double a = 0, b = 0;
-	struct drand48_data *buffer;
+	double a = 0;
+	double b = 0;
+	struct drand48_data buffer;
 	
 	for(int i = 0; i < y; i++){
-		drand48_r(buffer, a);
-		drand48_r(buffer, b);
+		drand48_r(&buffer, &a);
+		drand48_r(&buffer, &b);
 		
 		if( (a*a + b*b) < 1){
 			(*count)++;
@@ -36,4 +24,21 @@ void *f(void * arg){
 	}
 
 	return (void*)count;
+}
+
+int main (int argc, char *argv[]) 
+{
+
+	int shots = *(int*) argv[1];
+	double res;
+	if(argc > 2){
+		int threads = *(int*) argv[2];
+		pthread_t th2;
+		pthread_create(&th2, NULL, f, (void *) &shots);
+	}else{
+		res = *(double*)f((void*)&shots);
+	}
+
+	printf("%f\n", res*4);
+	return 0;
 }
